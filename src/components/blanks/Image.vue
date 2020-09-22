48e20799f58f3e7fec88d48e92a35ba99a89bd41
Aqualon <lashkin200936@gmail.com>
2020-09-22 11:29:10 +0300

    smthn

--- src/components/blanks/Image.vue
+++ src/components/blanks/Image.vue
@@ -14,6 +14,7 @@
             )
             //:src="fallbackSrc"
             img.lazyload(
+                data-sizes="auto"
                 :key="'image'"
                 :data-src="fallbackSrc"
                 :data-lowsrc="blur"
